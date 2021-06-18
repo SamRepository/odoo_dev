74da829dff10d0c5c615abc24c6082646db49592
SamRepository <samir.sellami@live.fr>
2021-06-18 19:02:34 +0100

    [UPDT] Change of the version to 12.0.0.2 and Modify README file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odoo_dev
+
+Module Odoo pour gérer les stages de perfectionnement au niveau de l'ENSET-Skikda
--- stages/__manifest__.py
+++ stages/__manifest__.py
@@ -11,7 +11,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Education',
-    'version': '12.0.0.1',
+    'version': '12.0.0.2',
     # any module necessary for this one to work correctly
     'depends': ['base'],
     'application': ['True'],
