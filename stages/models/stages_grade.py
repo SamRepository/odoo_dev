3fb04652ae1ef1307c67ed5815b059c310be082a
SamRepository <samir.sellami@live.fr>
2021-06-20 13:54:17 +0100

    [UPDT] The following models was updated: stages.enseignant stages.grade

--- stages/models/stages_grade.py
+++ stages/models/stages_grade.py
@@ -4,10 +4,10 @@ from odoo import models, fields, api,  _
 
 class StagesGrade(models.Model):
     _name = 'stages.grade'
-    _description = 'Grades'
+    _description = 'Grade Scientifique'
     _rec_name = 'name'
 
-    name = fields.Char(string="Intitulé", required=True)
-    categories = fields.Char(string="Catégories", required=False)
-    corps = fields.Char(string="Corps", required=False)
+    name = fields.Char(string="Intitulé grade / إسم الرتبة", required=True)
+    categories = fields.Char(string="Catégories / الفئة", required=False)
+    corps = fields.Char(string="Corps / السلك", required=False)
 
