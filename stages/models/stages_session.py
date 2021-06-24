1c92c5a510297e502cdd9ab83dee28664d75c393
SamRepository <samir.sellami@live.fr>
2021-06-24 01:47:57 +0100

    [ADD] Adding stages.candidature model [UPDT] Updating stages_docs_depart and stages_session

--- stages/models/stages_session.py
+++ stages/models/stages_session.py
@@ -3,11 +3,12 @@
 from odoo import models, fields, api,  _
 
 class StagesSession(models.Model):
-    _name = 'stages.session'
-    _description = 'Session Stages - فترات التربص'
-    _rec_name = 'name'
+    _inherit = ['event.event']
+    # _name = 'stages.session'
+    # _description = 'Session Stages - فترات التربص'
+    # _rec_name = 'name'
 
-    name = fields.Char(string="Intitulé - إسم الفترة", required=True)
-    exercice_id= fields.Many2one(comodel_name="stages.exercice", required=True, string="Excersice Fiscal - السنة المالية")
-    date_debut = fields.Date(string="Date debut - تاريخ البداية", required=True, )
-    date_fin = fields.Date(string="Date fin - تاريخ النهاية", required=True, )
+    # name = fields.Char(string="Intitulé - إسم الفترة", required=True)
+    # exercice_id= fields.Many2one(comodel_name="stages.exercice", required=True, string="Excersice Fiscal - السنة المالية")
+    # date_debut = fields.Date(string="Date debut - تاريخ البداية", required=True, )
+    # date_fin = fields.Date(string="Date fin - تاريخ النهاية", required=True, )
