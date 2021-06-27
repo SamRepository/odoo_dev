7f66fdda2a79a7977e59c5ab9f2c32aa81fe6208
SamRepository <samir.sellami@live.fr>
2021-06-27 01:09:29 +0100

    [UPDT] Update model res.partner : selection=[('company', 'Company')<=> ('person', 'Individual')], is_company = fields.Boolean(string='Is a Company', default=False =>True
    [UPDT] Update views views_main.xml/ stages_session_views.xml / stages_organisme_views.xml
    
    [UPDT] Update model stages_zone.py

--- stages/models/stages_zone.py
+++ stages/models/stages_zone.py
@@ -4,6 +4,3 @@ from odoo import models, fields, api,  _
 
 class StagesZone(models.Model):
     _inherit = ['res.country.group']
-
-
-    indemnite_jour = fields.Float(string="Indémnite Jour - علاوة المنطقة",  required=False, )
