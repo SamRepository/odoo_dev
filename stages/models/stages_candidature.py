b7b62e9fadf0e1e2fd249b36c4d099156282484a
SamRepository <samir.sellami@live.fr>
2022-03-19 00:22:29 +0100

    [ADD] Add "set_montant" method to stages_candidature.py which calculate stage montant depending on ('type_stage_id','zone','duree') by search in self.env['stages.type_stage.indemnite'] [UPDT] Update stages_candidature_views.xml

--- stages/models/stages_candidature.py
+++ stages/models/stages_candidature.py
@@ -30,16 +30,19 @@ class StagesCandidature(models.Model):
         for rec in self:
             if rec.partner_id:
                rec.pays = rec.partner_id.country_id.name
-               # rec.zone = rec.partner_id.country_id.country_group_ids.name
+
+    @api.depends('type_stage_id','zone','duree')
+    def set_montant(self):
+        for rec in self:
+               obj = self.env['stages.type_stage.indemnite'].search([('type_stage_id', '=',self.type_stage_id.name), ('zone_id.name', '=', self.zone), ('nombre_jour', '=', self.duree)])
+               rec.montant = obj.montant
 
     name = fields.Char(string='Référence', required=True, copy=False, readonly=True, default=lambda self: _('New'))
     employee_id =fields.Many2one(comodel_name="hr.employee", string="Enseignant", required=True,)
     session_id = fields.Many2one(comodel_name="event.event", string="Session Stage", required=True, )
     type_stage_id = fields.Many2one(comodel_name="stages.type_stage", string="Type Stage", required=True, readonly=True,)
     partner_id  = fields.Many2one(comodel_name="res.partner", string="Organisme d'accueil", required=True, )
- #   pays = fields.Many2one('res.country', related="partner_id.country_id", string="Pays", )
     pays = fields.Char(compute= 'set_pays', string="Pays", )
-    #zone_id =fields.Many2one('res.country.group', string='Zone', related='order_id.partner_id')
     contact = fields.Char(related="partner_id.child_ids.name", string="Contact", )
     zone = fields.Char(related="partner_id.country_id.country_group_ids.name", string="Zone", )
     state = fields.Selection(string="Etat Candidature",selection=[
@@ -49,10 +52,8 @@ class StagesCandidature(models.Model):
     date_depart = fields.Date(string="Date de départ", required=True, )
     date_retour = fields.Date(string="Date de retour ", required=True, )
     duree = fields.Integer(string = "Durée du Stage", compute= 'set_durre', store=True,)
-    # string = "Durée du Stage",
     cause_chg_period = fields.Char(string="Cause changement période", required=False, )
-    montant_bourse = fields.Float(string="Montant bourse",  required=False, )
-   # montant_bourse = fields.Float(compute="_compute_montat" ,string="Montant bourse", )
+    montant = fields.Float(string="Montant bourse", compute="set_montant", )
     objectifs_stage = fields.Text(string="Objectifs du stage", required=False, )
     methodologie = fields.Text(string="Méthodologie", required=False, )
     impacts_attendus  = fields.Text(string="Impacts attendus", required=False, )
@@ -70,40 +71,6 @@ class StagesCandidaturePerfectionnement(models.Model):
     date_1er_inscription = fields.Date(string="Date 1ère inscription", required=False, )
     nbr_inscription  = fields.Selection(string="Nbr d'inscription", selection=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'),('5', '5'), ('6', '6'),('+', 'plus de 6'),], required=False, )
 
-  # candidature_id = fields.Many2one(comodel_name="stages.candidature", string="Candidature", )
-  #  duree_bis = fields.Integer(compute='set_durre', readonly=True, store=False, )
-  #  pays_bis = fields.Many2one('res.country', related="partner_id.country_id", string="Pays", )
-  #  pays_bis = fields.Char(compute='set_pays', readonly=True, store=False, )
-    #zone_bis = fields.Char(compute='_set_pays', readonly=True, store=False, )
-
-    # @api.depends('date_retour', 'date_depart')
-    # def set_durre(self):
-    #     for rec in self:
-    #         if rec.date_depart:
-    #             if rec.date_retour:
-    #                 if rec.date_retour > rec.date_depart:
-    #                     rec.duree = (rec.date_retour - rec.date_depart).days
-    #                 else:
-    #                     raise UserError('La date de retour doit être supérieure à la date de départ')
-    #
-    # @api.depends('partner_id')
-    # def set_pays(self):
-    #     for rec in self:
-    #         if rec.partner_id:
-    #             rec.pays = rec.partner_id.country_id.name
-
-    # @api.depends('pays_bis')
-    # def _set_zone(self):
-    #     for rec in self:
-    #         if rec.pays_bis:
-    #            rec.zone_bis = "Zone1"
-
-    # @api.depends('type_stage_id','zone','durre')
-    # def _compute_montat(self):
-    #     for rec in self:
-    #         if rec.type_stage_id:
-    #            rec.montant_bourse = 1;
-
 class StagesCandidatureManifestation(models.Model):
     _name = 'stages.candidature.manifestation'
     _inherit = {'stages.candidature'}
@@ -117,12 +84,9 @@ class StagesCandidatureManifestation(models.Model):
         ('intervention', ' Intervention - مداخلة'),
         ('autre', 'Autre - شيء اخر'), ], required=False, )
 
-    # candidature_id = fields.Many2one(comodel_name="stages.candidature", string="Candidature", )
-
 
 class StagesCandidatureSejour(models.Model):
     _name = 'stages.candidature.sejour'
     _inherit = {'stages.candidature'}
     _description = "Séjour scientifique de haut niveau - إقامة علمية رفيعة المستوى بالخارج"
 
-    # candidature_id = fields.Many2one(comodel_name="stages.candidature", string="Candidature - طلب الترشح", )
