27678483506c8622d9ce8fcbddf0321d9be81198
SamRepository <samir.sellami@live.fr>
2022-04-29 16:27:38 +0100

    [ADD] name="active" widget="boolean_toggle" and  name="montant" sum="Totale des montants" decoration-bf="1" in the view stages_candidature_views.xml

--- stages/wizard/stages_candidature_chg_period.py
+++ stages/wizard/stages_candidature_chg_period.py
@@ -54,3 +54,64 @@ class ChangePeriodWizard(models.TransientModel):
     # def _default_country_id(self):
     #     return self.env.ref('base.DZD').id
 
+
+    # @api.model
+    # def default_get(self, fields):
+    #     res = super(ProductReplenish, self).default_get(fields)
+    #     product_tmpl_id = self.env['product.template']
+    #     if 'product_id' in fields:
+    #         if self.env.context.get('default_product_id'):
+    #             product_id = self.env['product.product'].browse(self.env.context['default_product_id'])
+    #             product_tmpl_id = product_id.product_tmpl_id
+    #             res['product_tmpl_id'] = product_id.product_tmpl_id.id
+    #             res['product_id'] = product_id.id
+    #         elif self.env.context.get('default_product_tmpl_id'):
+    #             product_tmpl_id = self.env['product.template'].browse(self.env.context['default_product_tmpl_id'])
+    #             res['product_tmpl_id'] = product_tmpl_id.id
+    #             res['product_id'] = product_tmpl_id.product_variant_id.id
+    #             if len(product_tmpl_id.product_variant_ids) > 1:
+    #                 res['product_has_variants'] = True
+    #     company = product_tmpl_id.company_id or self.env.company
+    #     if 'product_uom_id' in fields:
+    #         res['product_uom_id'] = product_tmpl_id.uom_id.id
+    #     if 'company_id' in fields:
+    #         res['company_id'] = company.id
+    #     if 'warehouse_id' in fields and 'warehouse_id' not in res:
+    #         warehouse = self.env['stock.warehouse'].search([('company_id', '=', company.id)], limit=1)
+    #         res['warehouse_id'] = warehouse.id
+    #     if 'date_planned' in fields:
+    #         res['date_planned'] = datetime.datetime.now()
+    #     return res
+    #
+    # def launch_replenishment(self):
+    #     uom_reference = self.product_id.uom_id
+    #     self.quantity = self.product_uom_id._compute_quantity(self.quantity, uom_reference)
+    #     try:
+    #         self.env['procurement.group'].with_context(clean_context(self.env.context)).run([
+    #             self.env['procurement.group'].Procurement(
+    #                 self.product_id,
+    #                 self.quantity,
+    #                 uom_reference,
+    #                 self.warehouse_id.lot_stock_id,  # Location
+    #                 _("Manual Replenishment"),  # Name
+    #                 _("Manual Replenishment"),  # Origin
+    #                 self.warehouse_id.company_id,
+    #                 self._prepare_run_values()  # Values
+    #             )
+    #         ])
+    #     except UserError as error:
+    #         raise UserError(error)
+    #
+    # def _prepare_run_values(self):
+    #     replenishment = self.env['procurement.group'].create({
+    #         'partner_id': self.product_id.with_company(self.company_id).responsible_id.partner_id.id,
+    #     })
+    #
+    #     values = {
+    #         'warehouse_id': self.warehouse_id,
+    #         'route_ids': self.route_ids,
+    #         'date_planned': self.date_planned,
+    #         'group_id': replenishment,
+    #     }
+    #     return values
+
