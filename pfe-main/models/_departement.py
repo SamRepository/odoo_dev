e9707475167847a4c5e0de537499d9beee1ca9e9
Samir SELLAMI <36329990+SamRepository@users.noreply.github.com>
2025-06-21 10:39:13 +0100

    Delete pfe-main directory

--- pfe-main/models/_departement.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class Department(models.Model):
-    _inherit = 'hr.department'
-
-    intitule_dep_ar = fields.Char(string='Intitulé dep Ar - اسم القسم بالعربية')
--- pfe-main/models/announce.py
+++
@@ -1,3 +0,0 @@
-from odoo import models
-class Announce(models.Model):
-    _inherit = 'event.event'
